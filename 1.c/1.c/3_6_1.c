8c42e7d3b90727eaddb861983f70d9b254c5d798
1938917626@qq.com <1938917626@qq.com>
2019-11-09 11:44:30 +0800

    创建个C语言小练习

---
+++ 1.c/1.c/3_6_1.c
@@ -0,0 +1,8 @@
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//void main() {
+//	int n;
+//	printf("将一整数转化为八进制和十六进制,请输入一整数：");
+//	scanf_s("%d", &n);
+//	printf("%d的八进制为：%o,十六进制为：%x", n, n, n);
+//}
---
+++ 1.c/1.c/3_6_3.c
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//main() {
+//	float a, b, c, s, area;
+//	printf("请输入三角形三边的长度");
+//	scanf_s("%f,%f,%f", &a,&b,&c);
+//	s = 1.0 / 2 * (a + b + c);
+//	area = sqrt(s * (s - a) * (s - b) * (s - c));
+//	printf("a=%7.2f,b=%7.2f,c=%7.2f,s=%7.2f\n",a,b,c,s);
+//	printf("area=%7.2f\n", area);
+//}
