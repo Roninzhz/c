d5750d861a79007b7935507cc8fe0b2e4b9615c5
1938917626@qq.com <1938917626@qq.com>
2019-11-09 13:52:21 +0800

    添加比较大小C语言练习

---
+++ 1.c/1.c/1_2_3.c
@@ -0,0 +1,22 @@
+//比较两者之间较大的数
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//void main() {
+//	int max(int x, int y);
+//	int a, b, c;
+//	scanf_s("%d%d", &a, &b);
+//	c = max(a, b);
+//	printf("max=%d", c);
+//}
+//int max(int x, int y) {
+//	int z;
+//	if (x>y)
+//	{
+//		z = x;
+//	}
+//	else
+//	{
+//		z = y;
+//	}
+//	return(z);
+//}
