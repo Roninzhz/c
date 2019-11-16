5d8fe5851047e55bf5ce6bb6ceacf7aa422ee8b0
1938917626@qq.com <1938917626@qq.com>
2019-11-16 15:43:48 +0800

    利用指针变量输入并输出10个数

---
+++ 1.c/1.c/8_2_3.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//void main() {
+//	int a[10], i, * p;
+//	for ( p=a; p<a+10; p++)
+//	{
+//		scanf("%d", p);
+//	}
+//	p = a;
+//	for ( i = 0; i < 10; i++)
+//	{
+//		printf("%d\t", *(p + i));
+//	}
+//}
