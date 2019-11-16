7f3f0968322cfb05272e28fbc8331e9d85c5e353
1938917626@qq.com <1938917626@qq.com>
2019-11-16 18:45:18 +0800

    指针

--- 1.c/1.c/10_1_1.c
+++ 1.c/1.c/10_1_1.c
@@ -1,4 +1,4 @@
-//#pragma warning(disable:4996)
+////#pragma warning(disable:4996)
 //#include <stdio.h>
 //void main() {
 //	struct stu /*定义结构*/
---
+++ 1.c/1.c/8_1_1.c
@@ -0,0 +1,12 @@
+//#pragma warning(disable:4996)
+//#include <stdio.h>
+//void main() {
+//	int a, b;
+//	int * pointer_1, * pointer_2;
+//	a = 100;
+//	b = 10;
+//	pointer_1 = &a;//ȡַ
+//	pointer_2 = &b;
+//	printf("%d,%d\n", a, b);
+//	printf("%d,%d\n", *pointer_1, *pointer_2);//ȡֵ
+//}
