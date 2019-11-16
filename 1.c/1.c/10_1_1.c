b27ab0d2d19ee14d63fa5de667a9031514ea4bad
1938917626@qq.com <1938917626@qq.com>
2019-11-16 18:23:32 +0800

    定义结构体

---
+++ 1.c/1.c/10_1_1.c
@@ -0,0 +1,14 @@
+//#pragma warning(disable:4996)
+//#include <stdio.h>
+//void main() {
+//	struct stu /*定义结构*/
+//	{
+//		int num;
+//		char  *name;
+//		char sex;
+//		float score;
+//	}boy2, boy1 = { 1,"ZhangHezhao",'M',100 };/*对boy1 初始化*/
+//	boy2 = boy1;
+//	printf("Number=%d\nName=%s\n", boy2.num, boy2.name);/*输出信息*/
+//	printf("Sex=%c\nScore=%f\n", boy2.sex, boy2.score);
+//}
