5c122680f53a4c215eabfaadb2067ea57d6ee389
1938917626@qq.com <1938917626@qq.com>
2019-11-16 18:06:10 +0800

    共用体变量赋名

---
+++ 1.c/1.c/10_2_1.c
@@ -0,0 +1,13 @@
+//#pragma warning(disable:4996)
+//#include <stdio.h>
+//union u
+//{
+//	char u1;
+//	int u2;
+//};
+//void main() {
+//	union u a = { 0x9741 };
+//	printf("1.%c %x\n", a.u1, a.u2);
+//	a.u1 = 'a';
+//	printf("2.%c %x\n", a.u1, a.u2);
+//}
