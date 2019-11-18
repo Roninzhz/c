c78f643d49562caa7a11474a5672572762a8bc52
1938917626@qq.com <1938917626@qq.com>
2019-11-18 08:35:30 +0800

    计算等差数列前n项和

--- 1.c/1.c/3_6_2.c
+++ 1.c/1.c/3_6_2.c
@@ -2,7 +2,7 @@
 //#pragma warning(disable:4996)
 //main() {
 //	int al, n, d, s;
-//	printf("计算等差数列前n项和,please input a1,n,d\n");
+//	printf("please input a1,n,d\n");
 //	scanf("%d,%d,%d", &al, &n, &d);
 //	s = al * n + (n - 1) * n * d / 2;
 //	printf("al=%d,n=%d,d=%d,s=%d", al, n, d, s);
