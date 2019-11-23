0b204915750c6d8c195db528102d75ce14ef9ecf
1938917626@qq.com <1938917626@qq.com>
2019-11-23 11:18:15 +0800

    不交换两变量的值，但交换指针指向，从大到小对两个数排序

---
+++ 1.c/1.c/8_1_2.c
@@ -0,0 +1,17 @@
+//#pragma warning(disable:4996)
+//#include <stdio.h>
+//main() {
+//	int a, b, * p, * p1, * p2;
+//	printf("Input two integer data:\n");
+//	scanf("%d%d", &a, &b);
+//	p1 = &a;
+//	p2 = &b;
+//	if (a<b)
+//	{
+//		p = p1;
+//		p1 = p2;
+//		p2 = p;
+//	}
+//	printf("max=%d,min=%d\n", *p1, *p2);
+//	printf("a=%d,b=%d", a, b);
+//}
