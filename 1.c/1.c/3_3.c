f7400721c451b095f52418633b1d20cba3527ecf
1938917626@qq.com <1938917626@qq.com>
2019-11-30 17:22:04 +0800

    编写fun（）求斐波那契数列中大于t的最小的数，当t=1000，fun（n）=1597

---
+++ 1.c/1.c/3_3.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//int fun(int t) {
+//	int i, f0 = 0, f1 = 1, f2 = f0 + f1;
+//	while (f2 <= t) {
+//		f0 = f1;
+//		f1 = f2;
+//		f2 = f0 + f1;
+//	}
+//	return f2;
+//}
+//void main() {
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", fun(n));
+//}
