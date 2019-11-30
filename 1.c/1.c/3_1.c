dad8c330baacff3303eaeb356a4c6cdde69712cf
1938917626@qq.com <1938917626@qq.com>
2019-11-30 17:12:35 +0800

    递归函数将整数n 逆序输出，主函数调用

--- 1.c/1.c/3_1.c
+++ 1.c/1.c/3_1.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-int sum(int);
-int powers(int);
-void main() {
-	int n;
-	n = 5;
-	printf("%d", sum(n));
-}
-int sum(int x) {
-	int i, s = 0;
-	for ( i = 1; i <= x; i++)
-	{
-		s += powers(i);
-	}
-	return s;
-}
-int powers(int y) {
-	int i, p;
-	p = 1;
-	/*for ( i = y; i <= y; i++)
-	{
-		p = y * 10 + y;
-	}*/
-	if (y<=5)
-	{
-		p = y * 10 + y;
-	}
-	return p;
-}
+//#include <stdio.h>
+//int sum(int);
+//int powers(int);
+//void main() {
+//	int n;
+//	n = 5;
+//	printf("%d", sum(n));
+//}
+//int sum(int x) {
+//	int i, s = 0;
+//	for ( i = 1; i <= x; i++)
+//	{
+//		s += powers(i);
+//	}
+//	return s;
+//}
+//int powers(int y) {
+//	int i, p;
+//	p = 1;
+//	/*for ( i = y; i <= y; i++)
+//	{
+//		p = y * 10 + y;
+//	}*/
+//	if (y<=5)
+//	{
+//		p = y * 10 + y;
+//	}
+//	return p;
+//}
---
+++ 1.c/1.c/3_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+void reverse(int n) {
+	if (n<=9)
+	{
+		printf("%d", n);//个位数直接输出
+	}
+	else
+	{
+		printf("%d", n % 10);//取余数并输出
+		reverse(n / 10);//将位数减少，并继续判断
+	}
+}
+void main() {
+	int n;
+	scanf("%d", &n);
+	reverse(n);//调用子函数
+}
