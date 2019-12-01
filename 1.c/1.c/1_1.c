1ba2bb59f2a672d12f9942ee9e7e26553f4fcbe6
1938917626@qq.com <1938917626@qq.com>
2019-12-01 09:35:33 +0800

    成绩排序

---
+++ 1.c/1.c/1_1.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//#pragma warning(disable:4996)
+//#define N 5
+//int main() {
+//	int a[N], i;
+//	float ave;
+//	void sort(int a[]);
+//	for ( i = 0; i < N; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	sort(a);
+//	for ( i = 0; i < N; i++)
+//	{
+//		printf("%4d", a[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+//void sort(int a[]) {
+//	int i, j, t;
+//	for ( i =0; i < N; i++)
+//	{
+//		for ( j = 0; j < N-1-i; j++)
+//		{
+//			if (a[j]<a[j+1])
+//			{
+//				t = a[j];
+//				a[j] = a[j + 1];
+//				a[j + 1] = t;
+//			}
+//		}
+//	}
+//}
