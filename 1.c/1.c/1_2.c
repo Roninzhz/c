f815698b389c7b7f750475aafa65f456fe1207e1
1938917626@qq.com <1938917626@qq.com>
2019-12-01 16:05:05 +0800

    输入正整数n，生成一张阶乘表，调用函数fact（n） 函数类型double

---
+++ 1.c/1.c/1_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+double fact(int n);
+void main() {
+	int i, n;
+	double result;
+	printf("enter n\n");
+	scanf("%d", &n);
+	for (i = 0; i <= n; i++) {
+		result = fact(i);
+		printf("%d!=%.0f\n", i, result);
+	}
+}
+double fact(int n) {
+	int i;
+	double product = 1;
+	for (i = 1; i <= n; i++) {
+		product *= i;
+	}
+	return product;
+}
